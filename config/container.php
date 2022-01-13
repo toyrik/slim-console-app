bf4aa83ef71f78c101ae101068578d3a717b42ad
toyrik <toyrik@gmail.com>
2022-01-13 15:27:23 +0300

    Added Vew command

--- config/container.php
+++ config/container.php
@@ -30,13 +30,6 @@ return [
 
     App::class => function (ContainerInterface $container) {
         $app = AppFactory::createFromContainer($container);
-
-        // Register routes
-        (require __DIR__ . '/routes.php')($app);
-
-        // Register middleware
-        (require __DIR__ . '/middleware.php')($app);
-
         return $app;
     },
 

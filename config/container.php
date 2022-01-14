9db71c873e44045c753235c3aab239ee50063b4d
toyrik <toyrik@gmail.com>
2022-01-14 08:44:39 +0300

    Added Users

--- config/container.php
+++ config/container.php
@@ -41,4 +41,18 @@ return [
         return new BasePathMiddleware($container->get(App::class));
     },
 
+    PDO::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['db'];
+
+        $host = $settings['host'];
+        $dbname = $settings['database'];
+        $username = $settings['username'];
+        $password = $settings['password'];
+        $charset = $settings['charset'];
+        $flags = $settings['flags'];
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+        return new PDO($dsn, $username, $password, $flags);
+    },
+
 ];
--- config/settings.php
+++ config/settings.php
@@ -30,4 +30,25 @@ $settings['error'] = [
     'log_error_details' => true,
 ];
 
+// Database settings
+$settings['db'] = [
+    'driver' => 'mysql',
+    'host' => 'mysql',
+    'username' => 'root',
+    'database' => 'slim',
+    'password' => 'root',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'flags' => [
+        // Turn off persistent connections
+        PDO::ATTR_PERSISTENT => false,
+        // Enable exceptions
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Emulate prepared statements
+        PDO::ATTR_EMULATE_PREPARES => true,
+        // Set default fetch mode to array
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ],
+];
+
 return $settings;
